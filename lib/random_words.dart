242444ed249fb8bb5e1c0721b9a8f513929ea1b3
Mariodev27 <mariosantisteban23@gmail.com>
2023-11-26 17:22:39 -0500

    ejemplo

---
+++ lib/random_words.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _sugestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Generador de nombres aleatorios'),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: _buildSugestions(),
+    );
+  }
+
+  Widget _buildSugestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
+        final index = i ~/ 2;
+        if (index >= _sugestions.length) {
+          _sugestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_sugestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(pair.asPascalCase, style: _biggerFont),
+    );
+  }
+}
